3fae26ba29dfee462fd9f52b459dd443306c8e3c
Muhammad Ofie <ofieisranta@gmail.com>
2023-11-10 20:32:51 +0700

    fix: create 3

--- src/app/api/mobile/profile/route.js
+++ src/app/api/mobile/profile/route.js
@@ -5,71 +5,71 @@ import { google } from "googleapis";
 import pkey from "../../../../../second-network-403402-9612f42f626d.json";
 import stream from 'stream';
 
-const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
+// const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
 
-async function authorize() {
-  const jwtClient = new google.auth.JWT(
-    pkey.client_email,
-    null,
-    pkey.private_key,
-    SCOPES
-  );
-  await jwtClient.authorize();
-  return jwtClient;
-}
+// async function authorize() {
+//   const jwtClient = new google.auth.JWT(
+//     pkey.client_email,
+//     null,
+//     pkey.private_key,
+//     SCOPES
+//   );
+//   await jwtClient.authorize();
+//   return jwtClient;
+// }
 
-async function downloadFile(authClient, fileName, fileId) {
-  const drive = google.drive({ version: 'v3', auth: authClient });
+// async function downloadFile(authClient, fileName, fileId) {
+//   const drive = google.drive({ version: 'v3', auth: authClient });
 
-  if (fileName && fileId) {
-    let dest = createWriteStream(fileName);
-    drive.files.get(
-      { fileId: fileId, alt: "media" },
-      { responseType: "stream" },
-      (err, { data }) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        data
-          .on("end", () => console.log("Done."))
-          .on("error", (err) => {
-            console.log(err);
-            return process.exit();
-          })
-          .pipe(dest);
-      }
-    );
-  }
-  else
-    console.log("Please specify file name/file id")
-}
+//   if (fileName && fileId) {
+//     let dest = createWriteStream(fileName);
+//     drive.files.get(
+//       { fileId: fileId, alt: "media" },
+//       { responseType: "stream" },
+//       (err, { data }) => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         data
+//           .on("end", () => console.log("Done."))
+//           .on("error", (err) => {
+//             console.log(err);
+//             return process.exit();
+//           })
+//           .pipe(dest);
+//       }
+//     );
+//   }
+//   else
+//     console.log("Please specify file name/file id")
+// }
 
-async function downloadFile(realFileId) {
-  // Get credentials and build service
-  // TODO (developer) - Use appropriate auth mechanism for your app
+// async function downloadFile(realFileId) {
+//   // Get credentials and build service
+//   // TODO (developer) - Use appropriate auth mechanism for your app
 
-  const {GoogleAuth} = require('google-auth-library');
-  const {google} = require('googleapis');
+//   const {GoogleAuth} = require('google-auth-library');
+//   const {google} = require('googleapis');
 
-  const auth = new GoogleAuth({
-    scopes: 'https://www.googleapis.com/auth/drive',
-  });
-  const service = google.drive({version: 'v3', auth});
+//   const auth = new GoogleAuth({
+//     scopes: 'https://www.googleapis.com/auth/drive',
+//   });
+//   const service = google.drive({version: 'v3', auth});
 
-  fileId = realFileId;
-  try {
-    const file = await service.files.get({
-      fileId: fileId,
-      alt: 'media',
-    });
-    console.log(file.status);
-    return file.status;
-  } catch (err) {
-    // TODO(developer) - Handle error
-    throw err;
-  }
-}
+//   fileId = realFileId;
+//   try {
+//     const file = await service.files.get({
+//       fileId: fileId,
+//       alt: 'media',
+//     });
+//     console.log(file.status);
+//     return file.status;
+//   } catch (err) {
+//     // TODO(developer) - Handle error
+//     throw err;
+//   }
+// }
 
 export async function GET(request) {
   console.log("--- Get my profile.");
@@ -87,7 +87,7 @@ export async function GET(request) {
       Object.entries(profile).filter(([key]) => !["password"].includes(key))
     );
 
-    imageUrl = await uploadFile(await authorize(), filename, buffer);
+    // imageUrl = await uploadFile(await authorize(), filename, buffer);
 
     return NextResponse.json(
       {
