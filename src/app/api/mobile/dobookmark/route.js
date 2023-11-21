812f567b018e1ea9c0fa7d1ce555f1ceee816381
Muhammad Ofie <ofieisranta@gmail.com>
2023-11-21 20:14:29 +0700

    fix: do/undo bookmark

--- src/app/api/mobile/dobookmark/route.js
+++ src/app/api/mobile/dobookmark/route.js
@@ -3,7 +3,6 @@ import { verifyToken } from "@/lib/authHelper";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  console.log("=== Do bookmark Vila. ");
   const requestHeaders = new Headers(request.headers);
   const bearerHeader = requestHeaders.get("authorization");
   const userData = verifyToken(bearerHeader);
@@ -22,19 +21,54 @@ export async function POST(request) {
       vilaId = formData.get("vilaId");
     }
 
-    await prisma.bookmarks.create({
-      data: {
+    let vila = await prisma.bookmarks.findFirst({
+      where: {
         vila_id: Number(vilaId),
         user_id: Number(userData.id),
       },
     });
 
-    return NextResponse.json(
-      {
-        message: `Bookmark Vila ${vilaId} success`,
-      },
-      { status: 200 }
-    );
+    if ( vila ) {
+      console.log("=== Undo bookmark Vila. ");
+      let undoBookmark = await prisma.bookmarks.deleteMany({
+        where: { 
+          vila_id: Number(vilaId), 
+          user_id: Number(userData.id), 
+        },
+      });
+
+      if ( undoBookmark ) {
+        return NextResponse.json({
+            message: `Undo bookmark success`,
+          }, { status: 200 }
+        );
+      } else {
+        return NextResponse.json({
+            message: `Undo bookmark failed`,
+          }, { status: 400 }
+        );
+      }
+    } else {
+      console.log("=== Do bookmark Vila. ");
+      let doBookmark = await prisma.bookmarks.create({
+        data: {
+          vila_id: Number(vilaId),
+          user_id: Number(userData.id),
+        },
+      });
+
+      if ( doBookmark ) {
+        return NextResponse.json({
+            message: `Do bookmark success`,
+          }, { status: 200 }
+        );
+      } else {
+        return NextResponse.json({
+            message: `Do bookmark failed`,
+          }, { status: 400 }
+        );
+      }
+    }
   } catch (error) {
     return NextResponse.json(error, { status: 500 });
   }
