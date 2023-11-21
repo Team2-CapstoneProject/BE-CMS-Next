e2805fbc4d614d5e3620c5e4f55e0d111dd86692
Muhammad Ofie <ofieisranta@gmail.com>
2023-11-21 19:01:56 +0700

    fix: bookmark list

--- src/app/api/mobile/bookmark/route.js
+++ src/app/api/mobile/bookmark/route.js
@@ -22,12 +22,33 @@ export async function GET(request) {
             name: true,
             location: true,
             price: true,
+            VilaImages: {
+              select: {
+                slider_image: true,
+              },
+              orderBy: {
+                id: "asc",
+              },
+              take: 1,
+            },
           },
         },
       },
       orderBy: {
         updatedAt: "desc",
       },
+    });
+
+    vilas = vilas.map((x) => {
+      return {
+        id: x.id,
+        updatedAt: x.updatedAt,
+        vila_id: x.Vilas.id,
+        vila_name: x.Vilas.name,
+        vila_location: x.Vilas.location,
+        vila_price: x.Vilas.price,
+        vila_image: x.Vilas.VilaImages.length > 0 ? x.Vilas.VilaImages[0].slider_image : null,
+      }
     });
 
     return NextResponse.json(
