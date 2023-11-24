7a1c52ef1d517c2b4c73f756872a09f0ad18ef3f
Muhammad Ofie <ofieisranta@gmail.com>
2023-11-24 12:03:31 +0700

    fix: jsx 7

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,10 @@
+"use client";
+
 import { Inter } from 'next/font/google'
-import ReduxProvider from './reduxprovider'
+
+import { Provider } from "react-redux";
+import Store from "./store";
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       {/* <body className={inter.className}>{children}</body> */}
       <body>
-        <ReduxProvider>{children}</ReduxProvider>
+        <Provider store={Store}>{children}</Provider>
       </body>
     </html>
   )
--- src/app/reduxprovider.js
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Provider } from "react-redux";
-import Store from "./store";
-
-export default function ReduxProvider({ children }) {
-  return <Provider store={Store}>{children}</Provider>;
-}
