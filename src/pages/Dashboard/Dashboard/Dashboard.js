e050e7792f66c597a24999d4f78c4c464b1bdfdb
Muhammad Ofie <ofieisranta@gmail.com>
2023-11-24 14:25:37 +0700

    fix: jsx 14

--- src/pages/Dashboard/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard/Dashboard.js
@@ -8,12 +8,12 @@ import ClipLoader from "react-spinners/ClipLoader";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
-  // const dashboardData = useSelector((state) => state.dashboard.data);
-  // const loading = useSelector((state) => state.dashboard.loading);
+  const dashboardData = useSelector((state) => state.dashboard.data);
+  const loading = useSelector((state) => state.dashboard.loading);
 
-  useEffect(() => {
-    dispatch(fetchDashboardData());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchDashboardData());
+  // }, [dispatch]);
 
   if (loading || !dashboardData) {
     return (
